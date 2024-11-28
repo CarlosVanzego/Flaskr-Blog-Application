b06ebbe82148d2f0084e63a3217bb0d1c3479d6d
Your Name <carlosvanzego96@gmail.com>
2024-11-28 03:23:14 -0600

    edits

--- flask-tutorial-new/flaskr/__init__.py
+++ flask-tutorial-new/flaskr/__init__.py
@@ -27,7 +27,7 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/hello')
     def hello():
-        return 'Hello, World! My name is Los.'
+        return 'Hello, World! My name is Los :)'
     
     from . import db
     db.init_app(app)
